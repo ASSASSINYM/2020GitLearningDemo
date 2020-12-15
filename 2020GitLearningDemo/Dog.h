7f58db6d028212485cc0637f5d47840a2e9ad6a3
ASSASSINYM <darlingiloveu@sina.com>
2020-12-15 10:12:40 +0800

    Dev 分支新增一个 dog 类

---
+++ 2020GitLearningDemo/Dog.h
@@ -0,0 +1,19 @@
+//
+//  Dog.h
+//  2020GitLearningDemo
+//
+//  Created by ASSASSIN on 2020/12/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Dog : NSObject
+
+@property(nonatomic,copy)NSString *name;
+@property(nonatomic,copy)NSString *age;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ 2020GitLearningDemo/Dog.m
@@ -0,0 +1,12 @@
+//
+//  Dog.m
+//  2020GitLearningDemo
+//
+//  Created by ASSASSIN on 2020/12/15.
+//
+
+#import "Dog.h"
+
+@implementation Dog
+
+@end
