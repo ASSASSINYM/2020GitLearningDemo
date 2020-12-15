3f14401594c711cb6a8c5559f0d110767de47e01
ASSASSINYM <darlingiloveu@sina.com>
2020-12-15 09:31:36 +0800

    Initial Commit

---
+++ 2020GitLearningDemoTests/_020GitLearningDemoTests.m
@@ -0,0 +1,36 @@
+//
+//  _020GitLearningDemoTests.m
+//  2020GitLearningDemoTests
+//
+//  Created by ASSASSIN on 2020/12/15.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _020GitLearningDemoTests : XCTestCase
+
+@end
+
+@implementation _020GitLearningDemoTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
